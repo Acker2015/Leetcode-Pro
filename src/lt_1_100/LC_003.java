53d512ce34a5d657d4850c5fbe7de347d5980b80
Acker <wanggaigezju@gmail.com>
2019-01-23 14:39:33 +0800

    feat: add 003&006

---
+++ src/lt_1_100/LC_003.java
@@ -0,0 +1,63 @@
+package lt_1_100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * 
+ *
+Example 1:
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Example 2:
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+遍历字符串，遇到已经出现过的字符就取上一个字符到现在位置的字符串长。
+注意
+1. 需要使用start标记上一次出现重复字符的地方，防止重复从头计算
+2. 出现重复字符，需要更新start标记
+
+two pointers
+
+ * @author Acker
+ *
+ */
+public class LC_003 {
+	public int lengthOfLongestSubstring(String s) {
+		if (s == null || s.length() <= 0) {
+			return 0;
+		}
+		Map<Character, Integer> locMap = new HashMap<>();
+		int index=0, start=0, maxLen=0;
+		while(index < s.length()) {
+			// make sure the area at right of start
+			if (locMap.containsKey(s.charAt(index)) && locMap.get(s.charAt(index)) >= start) {
+				maxLen = Math.max(maxLen, index - start);
+				start = locMap.get(s.charAt(index)) + 1;
+			}
+			locMap.put(s.charAt(index), index++);
+		}
+		return Math.max(maxLen, s.length() - start);
+    }
+	
+	public static void main(String[] args) {
+		LC_003 lc_003 = new LC_003();
+		System.out.println(lc_003.lengthOfLongestSubstring("abcabcbb"));
+		System.out.println(lc_003.lengthOfLongestSubstring("aab"));
+		System.out.println(lc_003.lengthOfLongestSubstring("abba"));
+		System.out.println(lc_003.lengthOfLongestSubstring("cdd"));
+		System.out.println(lc_003.lengthOfLongestSubstring("abcb"));
+	}
+
+}
---
+++ src/lt_1_100/LC_006.java
@@ -0,0 +1,52 @@
+package lt_1_100;
+/**
+ * https://leetcode.com/problems/zigzag-conversion/submissions/
+ * find the regular pattern
+ * row		gap1			gap2	
+ * 0  		2(row-1)		2 * 0 		（gap2=0需要跳过）
+ * 1			2*(row-2)	2 * 1
+ * 2			2*(row-3)	2 * 2
+ * ...
+ * row-1		0			2*(row-1) 	（gap1=0需要跳过）
+ *
+ * 
+ * @author Acker
+ *
+ */
+public class LC_006 {
+	
+	public String convert(String s, int numRows) {
+        if (s.length() <= 1 || numRows <= 1) return s;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < numRows && i < s.length(); ++i) {
+        		int gap1 = 2 * (numRows - i - 1);
+        		int gap2 = 2 * i;
+        		
+        		int left = i;
+        		stringBuilder.append(s.charAt(left));
+        		while (left < s.length()) {
+        			// （gap1=0需要跳过）
+        			if (i < numRows-1) {
+        				left += gap1;
+        				if (left < s.length()) {
+        					stringBuilder.append(s.charAt(left));
+        				}
+        			}
+        			// (gap2=0需要跳过）
+        			if (i > 0) {
+        				left += gap2;
+        				if (left < s.length()) {
+        					stringBuilder.append(s.charAt(left));
+        				}
+        			}
+        		}
+        }
+        return stringBuilder.toString();
+    }
+
+	public static void main(String[] args) {
+		LC_006 lc_006 = new LC_006();
+		System.out.println(lc_006.convert("PAYPALISHIRING", 3));
+		System.out.println(lc_006.convert("PAYPALISHIRING", 4));
+	}
+}
